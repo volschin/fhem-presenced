47cb24c8a30ad8d22fc7825a399d355c3587d2fe
volschin <volschin@users.noreply.github.com>
2023-01-15 11:41:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# start services
+sudo service dbus start
+sudo service bluetooth start
+
+# wait for startup of services
+msg="Waiting for services to start..."
+time=0
+echo -n $msg
+while [[ "$(pidof start-stop-daemon)" != "" ]]; do
+    sleep 1
+    time=$((time + 1))
+    echo -en "\r$msg $time s"
+done
+echo -e "\r$msg done! (in $time s)"
+
+# reset bluetooth adapter by restarting it
+sudo hciconfig hci0 down
+sudo hciconfig hci0 up
+
+# start application
 echo "Starte FHEM presenced"
 rm -f /run/presenced.pid
 exec /usr/bin/perl /presenced -v -p 5111
